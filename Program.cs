89dc94cf2218d7567e49e38d45309eea9f36a1a4
TylerRice27 <Tylerrayrice2@gmail.com>
2022-07-18 14:49:05 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        bool playing = true;
+        while (playing)
+        {
+            // Game Logic ...
+
+            Console.WriteLine("Hello, Welcome to Rock, Paper, Scissors Game. Enter your choice below.");
+            Console.WriteLine("Rock");
+            Console.WriteLine("Paper");
+            Console.WriteLine("Scissors");
+            Console.ReadLine();
+
+            Random randomChoice = new Random();
+            string keepPlaying = Console.ReadKey();
+            if (keepPlaying == 'n')
+            {
+                playing = false;
+            }
+            playing = true;
+        }
+
+    }
+}
